fa23fb7f4edb0a7f50de1de430d2d7b36f1c8422
Dylan Lee <leedylan0918@gmail.com>
2023-10-09 15:43:14 +0800

    Added a searchbar and carousel component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
+      <section className='px-6 md:px-20 py-24'>
         <div className='flex max-xl:flex-col gap-16'>
           <div className='flex flex-col justify-center'>
             <p className='small-text'>
@@ -16,18 +18,19 @@ const Home = () => {
                 height={16}
               />
             </p>
-
             <h1 className='head-text'>
               Unleashed the Power of
               <span className='text-primary'> PriceWise</span>
             </h1>
-
             <p className='mt-6'>
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
+
+            <Searchbar />
           </div>
-          HeroCarousel
+
+          <HeroCarousel />
         </div>
       </section>
 
